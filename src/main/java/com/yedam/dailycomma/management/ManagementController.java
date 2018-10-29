66ad891edbd38e6130108229968dee3a9decea68
jipro <wona23@gmail.com>
2018-10-30 02:24:42 +0900

    컨트롤러 파일명 변경 : manageController -> ManagementController

--- src/main/java/com/yedam/dailycomma/management/ManagementController.java
+++ src/main/java/com/yedam/dailycomma/management/ManagementController.java
@@ -1,23 +1,14 @@
 package com.yedam.dailycomma.management;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-import com.yedam.dailycomma.reservation.ReservationDTO;
 
 /**
  * Handles requests for the application home page.
  */
 @Controller
-public class manageController {
+public class ManagementController {
 
 	@RequestMapping("/stats.do")
 	public String stats(Model model /*ReservationDTO dto*/) {
