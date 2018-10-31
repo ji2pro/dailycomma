0af5901268a4f55573f9e9fe2bcb059392dc096e
jipro <wona23@gmail.com>
2018-10-31 09:39:30 +0900

    예약기능 관련 코드 추가 - 기능 미구현

--- src/main/java/com/yedam/dailycomma/reservation/ReservationDAOMybatis.java
+++ src/main/java/com/yedam/dailycomma/reservation/ReservationDAOMybatis.java
@@ -1,41 +1,42 @@
 package com.yedam.dailycomma.reservation;
 
-import java.util.List;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class ReservationDAOMybatis {	
 	
 	@Autowired SqlSessionTemplate mybatis;
 	//전체조회
-	public List<ReservationDTO> getUsers(ReservationSearchDTO searchDTO){
-		System.out.println("user mybatis 전체 조회========");
-		return mybatis.selectList("user.getUsers",searchDTO);
+	public List<ReservationDTO> getReservations(ReservationSearchDTO searchDTO) {
+		System.out.println("reservation mybatis 전체 조회========");
+		return mybatis.selectList("reservation.getReservations",searchDTO);
 	}
 	//건수 조회
 	public int getCnt(ReservationSearchDTO searchDTO) {
-		return mybatis.selectOne("user.getCnt",searchDTO);
+		return mybatis.selectOne("reservation.getCnt",searchDTO);
 	}
-	//단건조회
-	public ReservationDTO getUser(ReservationDTO dto){
-		System.out.println("user mybatis 단건 조회========");
-		return mybatis.selectOne("user.getUser", dto);
+	//단건 조회
+	public ReservationDTO getReservation(ReservationDTO dto) {
+		System.out.println("reservation mybatis 단건 조회========");
+		return mybatis.selectOne("reservation.getReservation", dto);
 	}
 	//등록
-	public int insertUser(ReservationDTO dto) {
-		System.out.println("user mybatis 등록========");
-		return mybatis.insert("user.insertUser", dto);
+	public int insertReservation(ReservationDTO dto) {
+		System.out.println("reservation mybatis 등록========");
+		return mybatis.insert("reservation.insertReservation", dto);
 	}
 	//수정
-	public int updateUser(ReservationDTO dto) {
-		System.out.println("user mybatis 수정========");
-		return mybatis.update("user.updateUser", dto);
+	public int updateReservation(ReservationDTO dto) {
+		System.out.println("reservation mybatis 수정========");
+		return mybatis.update("reservation.updateReservation", dto);
 	}
 	//삭제
-	public int deleteUser(ReservationDTO dto) {
-		System.out.println("user mybatis 삭제========");
-		return mybatis.delete("user.deleteUser", dto);
+	public int deleteReservation(ReservationDTO dto) {
+		System.out.println("reservation mybatis 삭제========");
+		return mybatis.delete("reservation.deleteReservation", dto);
 	}
 }
--- src/main/java/com/yedam/dailycomma/reservation/ReservationServiceImpl.java
+++ src/main/java/com/yedam/dailycomma/reservation/ReservationServiceImpl.java
@@ -1,8 +1,9 @@
 package com.yedam.dailycomma.reservation;
 
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service //빈등록
 public class ReservationServiceImpl implements ReservationService {
@@ -13,38 +14,32 @@ public class ReservationServiceImpl implements ReservationService {
 	//@Autowired UserDAO dao;
 	//@Autowired UserDAOSpring dao;
 	@Autowired ReservationDAOMybatis dao;
-		
-	@Override
-	public int insertUser(ReservationDTO dto) {		
-		return dao.insertUser(dto); // 두번의 인설트를 만들어 일부러 에러를 만듬 트랜잭션 처리를 하게 되면 둘다 인설트가 되지 않게 한다.
+
+	public int insertReservation(ReservationDTO dto) {
+		return dao.insertReservation(dto); // 두번의 인설트를 만들어 일부러 에러를 만듬 트랜잭션 처리를 하게 되면 둘다 인설트가 되지 않게 한다.
 	}
 
-	@Override
-	public ReservationDTO getUser(ReservationDTO dto) {
+	public ReservationDTO getReservation(ReservationDTO dto) {
 		//logAdvice.printLogging();	//횡단관심
 		System.out.println("사용자 목록 조회");
 		//int a = 5/0; //강제로 예외 발생
-		return dao.getUser(dto);	//핵심관심
+		return dao.getReservation(dto);	//핵심관심
 	}
 
-	@Override
-	public List<ReservationDTO> getUsers(ReservationSearchDTO searchDTO) {
+	public List<ReservationDTO> getReservations(ReservationSearchDTO searchDTO) {
 		//int a = 5/0; //강제로 예외 발생
 		//logAdvice.printLogging();	//횡단관심
-		return dao.getUsers(searchDTO);		//핵심관심
+		return dao.getReservations(searchDTO);		//핵심관심
 	}
 
-	@Override
-	public int updatetUser(ReservationDTO dto) {	
-		return dao.updateUser(dto);
+	public int updatetReservation(ReservationDTO dto) {
+		return dao.updateReservation(dto);
 	}
 
-	@Override
-	public int deleteUser(ReservationDTO dto) {
-		return dao.deleteUser(dto);
+	public int deleteReservation(ReservationDTO dto) {
+		return dao.deleteReservation(dto);
 	}
 
-	@Override
 	public int getCnt(ReservationSearchDTO searchDTO) {
 		return dao.getCnt(searchDTO);
 	}
