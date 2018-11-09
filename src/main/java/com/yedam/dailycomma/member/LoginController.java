8893b942773900081c4e803ad6ef0bf108f3d08e
JunWoo Son <sjw3957@gmail.com>
2018-11-09 18:15:47 +0900

    적립금 적용 완료

--- src/main/java/com/yedam/dailycomma/member/LoginController.java
+++ src/main/java/com/yedam/dailycomma/member/LoginController.java
@@ -1,10 +1,13 @@
 package com.yedam.dailycomma.member;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -36,6 +39,7 @@ public class LoginController {
 	
 	@RequestMapping("/loginForm.do")
 	public String loginForm() {
+		//System.out.println("page===================== "+page);
 		return "member/memberLogin";
 	}	
 	
