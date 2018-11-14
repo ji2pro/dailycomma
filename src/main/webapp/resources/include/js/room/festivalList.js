70d744e1a4fe37de2d3340f31fa274a74cb40fdf
hyeonuk <akacia900@gmail.com>
2018-11-14 19:32:38 +0900

    festival ui

--- src/main/webapp/resources/include/js/room/festivalList.js
+++ src/main/webapp/resources/include/js/room/festivalList.js
@@ -75,22 +75,52 @@ function detailFestival(contentid) {
                             </div><!--// sub-detail-slide -->
                             </div><!--// left-content -->*/
 
-                "<div class='carousel slide' data-ride='carousel' id='carousel2'>" +
+                /*"<div class='carousel slide' data-ride='carousel' id='carousel2'>" +
+                "<div class='carousel-inner'>";*/
+                "<div id='carouselExampleControls' class='carousel slide' data-ride='carousel'>" +
                 "<div class='carousel-inner'>";
+ 
             $.each(datas.items3.items.item, function (idx, datas) {
                 detailFestival +=
                     "<div class='carousel-item active'>" +
                     "<img class='d-block img-fluid w-100' src='" + datas.originimgurl + "'/>" +
                     "</div>";
             })
+            
             detailFestival +=
                 "</div>" +
-                "<a class='carousel-control-prev' href='#carousel2' role='button' data-slide='prev'>" +
-                "<span class='carousel-control-prev-icon'></span></a>" +
-                "<a class='carousel-control-next' href='#carousel2' role='button' data-slide='next'>" +
-                "<span class='carousel-control-next-icon'></span> </a>" +
+                "<a class='carousel-control-prev' href='#carouselExampleControls' role='button' data-slide='prev'>" +
+                "<span class='carousel-control-prev-icon' aria-hidden='true'></span></a><span class='sr-only'>이전</span>" +
+                "<a class='carousel-control-next' href='##carouselExampleControls' role='button' data-slide='next'>" +
+                "<span class='carousel-control-next-icon' aria-hidden='true'></span><span class='sr-only'>다음</span> </a>" +
                 "</div>" +
-
+                /*
+                "<div id='carouselExampleControls' class='carousel slide' data-ride='carousel'>" +
+                "<div class='carousel-inner'>"
+                  <div class="carousel-item active">
+                    <img class="d-block w-100" src="..." alt="첫번째 슬라이드">
+                  </div>
+                  <div class="carousel-item">
+                    <img class="d-block w-100" src="..." alt="두번째 슬라이드">
+                  </div>
+                  <div class="carousel-item">
+                    <img class="d-block w-100" src="..." alt="세번째 슬라이드">
+                  </div>
+                </div>
+                <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span class="sr-only">이전</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span class="sr-only">다음</span>
+                </a>
+              </div>*/
+                
+                
+                
+                
+                "</div>" + 
                 <!-- right-content -->
                 "<aside class='right-content'>" +
                 "<div class='aside' style='width: auto; top: auto; height: auto; bottom: auto;'>" + <!-- scroll fixed 추가 -->
