eb72f7ae90755f8fd0e2b7ce79bbcb360bfed44c
JunYoung <ko5642027@naver.com>
2018-11-18 19:50:38 +0900

    메인 날짜 예외 처리 완료

--- src/main/webapp/resources/include/js/lodgment/Search.js
+++ src/main/webapp/resources/include/js/lodgment/Search.js
@@ -58,7 +58,7 @@ $(document).ready(function() {
          onClose: function( selectedDate ) {
              // 시작일(fromDate) datepicker가 닫힐때
              // 종료일(toDate)의 선택할수있는 최소 날짜(minDate)를 선택한 시작일로 지정
-             $("#datepicker").datepicker( "option", "minDate", selectedDate );
+             $("#datepicker").datepicker( "option", "maxDate", selectedDate );
          }
 	 });
      
